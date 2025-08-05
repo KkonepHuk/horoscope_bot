31736969139036b5b5d0ad884d9df0205f347e10
Никитос <nikitos@MacBook-Air-Tana.local>
2025-08-05 10:12:16 +0300

    Bug fix in sending daily message

--- cron/send_horoscopes.py
+++ cron/send_horoscopes.py
@@ -11,6 +11,6 @@ async def send_all_horoscopes_to_all_users():
     album1, album2 = create_album()
     for chat_id in chat_ids:
         daily_message = random.choice(DAILY_MESSAGES)
-        bot.send_message(chat_id, daily_message)
+        await bot.send_message(chat_id=chat_id, text=daily_message)
         await bot.send_media_group(chat_id=chat_id, media=album1)
         await bot.send_media_group(chat_id=chat_id, media=album2)
