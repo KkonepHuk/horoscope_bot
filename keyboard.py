27a7ec8e411504862c5a1df6fe381fcb2f99b493
Никитос <nikitos@MacBook-Air-Tana.local>
2025-08-12 21:14:59 +0300

    Add zodiac_keyboard

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,7 @@
 #from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import types
+from horoscope_maker.core.consts import SIGNS, ENG_TO_RUS
+from database.db_operations import get_zodiac_signs_for_user
 
 def daily_keyboard():
     kb = [
@@ -24,3 +26,22 @@ def menu_keyboard():
 
     return menu
 
+async def zodiac_keyboard(user_id):
+    #Создаёт клавиатуру с выделением выбранных знаков
+    user_selected_signs = await get_zodiac_signs_for_user(user_id)
+    buttons = []
+    for sign in SIGNS:  # список всех знаков зодиака
+        russion_sign = ENG_TO_RUS[sign].capitalize()
+        if sign in user_selected_signs:
+            button_text = f"✅ {russion_sign}"
+        else:
+            button_text = f"  {russion_sign}"
+        buttons.append(types.InlineKeyboardButton(text=button_text, callback_data=f"toggle_{sign}"))
+    
+    buttons.append(types.InlineKeyboardButton(text="Выбрать все", callback_data="select_all"))
+    buttons.append(types.InlineKeyboardButton(text="Сбросить все", callback_data="deselect_all"))
+
+    kb = [buttons[i:i+3] for i in range(0, len(buttons), 3)]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=kb)
+
+    return keyboard
